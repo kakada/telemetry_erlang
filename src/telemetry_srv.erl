e97bca68cfdf0da3dcae5e0d2a10c729412aebb3
Juan Edi <jedi@manas.com.ar>
2015-09-18 11:57:07 -0300

    instedd/telemetry_rails#4: Buffer commands received when no connection is available

--- src/telemetry_srv.erl
+++ src/telemetry_srv.erl
@@ -1,7 +1,8 @@
 -module(telemetry_srv).
 -behaviour(gen_server).
 -define(SERVER, ?MODULE).
--record(state, {sock, connected}).
+-define(QUEUE_BOUND, 10).
+-record(state, {sock, connected, queue, queue_size}).
 %% ------------------------------------------------------------------
 %% API Function Exports
 %% ------------------------------------------------------------------
@@ -28,35 +29,41 @@ start_link() ->
 
 init([]) ->
   gen_server:cast(?SERVER, connect),
-  {ok, #state{connected = false}}.
+  {ok, #state{connected = false, queue = queue:new(), queue_size = 0}}.
 
 handle_call(_Request, _From, State) ->
   {reply, ok, State}.
 
-handle_cast({report,{_Command, _Args}}, State = #state{connected = false}) ->
-  % TO-DO: buffer commands until connection is available?
-  {noreply, State};
+handle_cast({report, R}, State = #state{connected = false, queue = Q0, queue_size = S0}) ->
+  {Q1, S1} = case S0 == ?QUEUE_BOUND of
+               true  ->
+                 {queue:in(R, queue:drop(Q0)), S0};
+               false ->
+                 {queue:in(R, Q0), S0 + 1}
+             end,
+  {noreply, State#state{queue = Q1, queue_size = S1}};
 
 handle_cast({report,{Command, Args}}, State = #state{connected = true, sock = Socket}) ->
   ok = gen_tcp:send(Socket, serialize_command(Command, Args)),
   ok = gen_tcp:send(Socket, "\n"),
   {noreply, State};
 
-handle_cast(connect, State = #state{connected = false}) ->
+handle_cast(connect, State = #state{connected = false, queue = Queue}) ->
   Host = "localhost",
   Port = 1234,
 
   case gen_tcp:connect(Host, Port, [binary, {delay_send, false}, {nodelay, true}, {buffer, 5}, {active, true}]) of
     {ok, Sock} ->
-      {noreply, #state{sock = Sock, connected = true}};
+      clear_queue(Queue),
+      {noreply, State#state{sock = Sock, connected = true, queue = queue:new(), queue_size = 0}};
     _ ->
       {ok, _} = timer:apply_after(1000, gen_server, cast, [?SERVER, connect]),
       {noreply, State}
   end.
 
-handle_info({tcp_closed, _}, _State) ->
+handle_info({tcp_closed, _}, State) ->
   gen_server:cast(?SERVER, connect),
-  {noreply, #state{connected = false}};
+  {noreply, State#state{connected = false}};
 
 handle_info(_Info, State) ->
   {noreply, State}.
@@ -82,3 +89,12 @@ serialize_command(Command, Args) ->
   ]},
 
   jiffy:encode(Json).
+
+clear_queue(Q) ->
+  case queue:is_empty(Q) of
+    true  -> ok;
+    false ->
+      {{value, R}, Q1} = queue:out(Q),
+      gen_server:cast(?SERVER, {report, R}),
+      clear_queue(Q1)
+  end.
